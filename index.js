1bbf3ec613b8a71e736b7f1b35ab7956b158ddca
gianlucarobotti200 <gianlucarobotti200@gmail.com>
2022-08-03 14:53:21 -0300

    Integración API DolarSi

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-function getDolars(){
+
+const listValues = async () => {
+    const dolarDia = await fetch('https://www.dolarsi.com/api/api.php?type=dolar');
+    const values = await dolarDia.json();
+    console.log("funcion listValues: " + values[0].casa.venta)
+    return((values[0].casa.venta))
+};
+
+window.addEventListener("load", function(){
+    listValues();
+})
+
+async function getDolars(){
     
-    let dolar = 132.19;
+    let dolar = await listValues();
+    dolar = parseFloat(dolar.replace(",", "."))
+    console.log("funcion getDolars " + dolar)
     
     if (document.getElementById('money').value == "") {
         alert("Completa el espacio en blanco")
     }
     
-    let pesos = (parseFloat(document.getElementById('money').value))*dolar;
+    let pesos = ((parseFloat(document.getElementById('money').value))*dolar).toFixed(2);
     let impPais = (pesos*.30).toFixed(2);
     let ret = (pesos*.45).toFixed(2);
     let pesosImpuestos = ((pesos*175)/100).toFixed(2);
@@ -16,3 +30,4 @@ function getDolars(){
     document.dolartospend.ret.value = ret;
     document.dolartospend.total.value = pesosImpuestos;
 }
+
