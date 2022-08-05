56e92cd36ddd036cdca6af93e84ec9fe68f3969f
gianlucarobotti200 <gianlucarobotti200@gmail.com>
2022-08-05 17:49:12 -0300

    función de elegir que moneda usar

--- index.js
+++ index.js
@@ -7,12 +7,30 @@ const listValues = async () => {
     console.log("funcion listValues: " + values[0].casa.venta)
     dolar = (parseFloat(values[0].casa.venta.replace(",", "."))).toFixed(2);
     let objDolar = document.getElementById("cotizacion");
-    objDolar.innerText = `Dólar hoy: $${dolar}`
+    objDolar.innerText = `Dólar Oficial hoy: $${dolar}`
 };
 
 window.addEventListener("load", function(){
     listValues();
 })
+
+function getPesos(){
+    let pesos = ((parseFloat(document.getElementById('money').value))).toFixed(2);
+    let objPesos = document.getElementById("pesosinimp");
+    objPesos.innerText = `Pesos sin impuesto: $${pesos}`;
+    console.log("funcion getPesos " + pesos);
+    let impPais = (pesos*.30).toFixed(2);
+    let objImpPais = document.getElementById("imppais");
+    objImpPais.innerText = `Impuesto país 30%: $${impPais}`;
+
+    let ret = (pesos*.45).toFixed(2);
+    let objRet = document.getElementById("ret");
+    objRet.innerText = `Retención del 45%: $${ret}`;
+
+    let total = ((pesos*175)/100).toFixed(2);
+    let objTotal = document.getElementById("total");
+    objTotal.innerText = `Total con impuestos: $${total}`;
+}
 
 function getDolars(){
     
@@ -38,3 +56,19 @@ function getDolars(){
     let objTotal = document.getElementById("total");
     objTotal.innerText = `Total con impuestos: $${total}`;   
 }
+
+document.getElementById("ars").addEventListener("click", function(){
+    console.log("holis");
+    let inPesos = document.querySelector(".in-dollar");
+    inPesos.placeholder = "Ingresar pesos";
+    inPesos.setAttribute("onchange", "getPesos()");
+    
+})
+
+document.getElementById("usd").addEventListener("click", function(){
+    console.log("holis");
+    let inDolars = document.querySelector(".in-dollar");
+    inDolars.placeholder = "Ingresar dólares";
+    inDolars.setAttribute("onchange", "getDolars()");
+    
+})
