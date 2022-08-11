016cd92500ef446c48dc22a2f6d7f321ef03814f
gianlucarobotti200 <gianlucarobotti200@gmail.com>
2022-08-11 16:09:50 -0300

    Actualizacion visual al select, y adicion de views

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const listValues = async () => {
     
     const dolarDia = await fetch('https://www.dolarsi.com/api/api.php?type=dolar');
     const values = await dolarDia.json();
-    console.log("funcion listValues: " + values[0].casa.venta)
-    dolar = (parseFloat(values[0].casa.venta.replace(",", "."))).toFixed(2);
+    console.log("funcion listValues: " + values[0].casa.compra)
+    dolar = (parseFloat(values[0].casa.compra.replace(",", "."))).toFixed(2);
     let objDolar = document.getElementById("cotizacion");
     objDolar.innerText = `Dólar Oficial hoy: AR$ ${dolar}`
 
@@ -16,10 +16,39 @@ window.addEventListener("load", function(){
 })
 
 function getPesos(){
-    let pesos = ((parseFloat(document.getElementById('money').value))).toFixed(2);
+
+    if (document.getElementById('money').value == "") {
+        alert("Completa el espacio en blanco");
+    } else {
+        let pesos = ((parseFloat(document.getElementById('money').value))).toFixed(2);
+        let objPesos = document.getElementById("pesosinimp");
+        objPesos.innerText = `Pesos sin impuesto: AR$ ${pesos}`;
+        console.log("funcion getPesos " + pesos);
+        let impPais = (pesos*.30).toFixed(2);
+        let objImpPais = document.getElementById("imppais");
+        objImpPais.innerText = `Impuesto país 30%: AR$ ${impPais}`;
+
+        let ret = (pesos*.45).toFixed(2);
+        let objRet = document.getElementById("ret");
+        objRet.innerText = `Retención del 45%: AR$ ${ret}`;
+
+        let total = ((pesos*175)/100).toFixed(2);
+        let objTotal = document.getElementById("total");
+        objTotal.innerText = `Total con impuestos: AR$ ${total}`;
+    }
+}
+
+function getDolars(){
+    
+    console.log("funcion getDolars " + dolar);
+    
+    if (document.getElementById('money').value == "") {
+        alert("Completa el espacio en blanco");
+    } else {
+        let pesos = ((parseFloat(document.getElementById('money').value))*dolar).toFixed(2);
     let objPesos = document.getElementById("pesosinimp");
     objPesos.innerText = `Pesos sin impuesto: AR$ ${pesos}`;
-    console.log("funcion getPesos " + pesos);
+    
     let impPais = (pesos*.30).toFixed(2);
     let objImpPais = document.getElementById("imppais");
     objImpPais.innerText = `Impuesto país 30%: AR$ ${impPais}`;
@@ -31,31 +60,7 @@ function getPesos(){
     let total = ((pesos*175)/100).toFixed(2);
     let objTotal = document.getElementById("total");
     objTotal.innerText = `Total con impuestos: AR$ ${total}`;
-}
-
-function getDolars(){
-    
-    console.log("funcion getDolars " + dolar);
-    
-    if (document.getElementById('money').value == "") {
-        alert("Completa el espacio en blanco");
-    }
-    
-    let pesos = ((parseFloat(document.getElementById('money').value))*dolar).toFixed(2);
-    let objPesos = document.getElementById("pesosinimp");
-    objPesos.innerText = `Pesos sin impuesto: AR$ ${pesos}`;
-    
-    let impPais = (pesos*.30).toFixed(2);
-    let objImpPais = document.getElementById("imppais");
-    objImpPais.innerText = `Impuesto país 30%: AR$ ${impPais}`;
-
-    let ret = (pesos*.45).toFixed(2);
-    let objRet = document.getElementById("ret");
-    objRet.innerText = `Retención del 45%: AR$ ${ret}`;
-
-    let total = ((pesos*175)/100).toFixed(2);
-    let objTotal = document.getElementById("total");
-    objTotal.innerText = `Total con impuestos: AR$ ${total}`;   
+    }  
 }
 
 document.getElementById("moneda").addEventListener("click", function(){
