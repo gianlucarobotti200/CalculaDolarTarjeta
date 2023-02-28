c54306958d8b87878cd6e363027826fe6dab2e1b
gianlucarobotti200 <gianlucarobotti200@gmail.com>
2023-02-28 16:54:05 -0300

    calculateDollar y calculatePesos terminado

---
+++ js/functions.js
@@ -0,0 +1,74 @@
+
+let calculateCountryTax = (value) => {
+    return Number(value*0.3.toFixed(3));
+}
+
+let calculateRetGanBPP = (value, moreThan300) => {
+    
+    let retGanBBPP;
+    if(moreThan300){
+        retGanBBPP = Number(value*0.7.toFixed(3));
+        document.querySelector("span#ret").innerText = "70";
+        //valueList[2].innerText = `$${retGanBBPP}`;
+
+        return retGanBBPP;
+    } else{
+        retGanBBPP = Number(value*0.45.toFixed(3));
+        document.querySelector("span#ret").innerText = "45";
+        //valueList[2].innerText = `$${retGanBBPP}`;
+
+        return retGanBBPP;
+    }
+
+}
+
+let calculatePercIIBB = (value, provinceNum,provincePercent) =>{
+    
+    switch(provinceNum){
+        case "1":
+            provincePercent.innerText = 0;
+            return  0.00;
+        case "2":
+        case "3":
+            
+            provincePercent.innerText = 2;
+            return Number(value*0.02.toFixed(3));
+        case "4":
+            provincePercent.innerText = 3;
+            return Number(value*0.03.toFixed(3));
+            
+        case "5":
+            provincePercent.innerText = 1;
+            return Number(value*0.01.toFixed(3));
+            
+        case "6":
+            provincePercent.innerText = 5;
+            return Number(value*0.05.toFixed(3));
+            
+        case "7":
+            provincePercent.innerText = 3.6;
+            return Number(value*0.036.toFixed(3));
+            
+        case "8":
+            provincePercent.innerText = 5.5;
+            return Number(value*0.055.toFixed(3));
+            
+        case "9":
+            provincePercent.innerText = 3;
+            return Number(value*0.03.toFixed(3));
+            
+    }
+}
+
+let calculateSealTax = (value, payMethodNum) =>{
+
+    if(payMethodNum=='1'){
+        document.querySelector('span#sello').innerText = 1.2
+        return Number(value*0.012.toFixed(3))
+        
+    } else{
+        document.querySelector('span#sello').innerText = 0
+        return 0.00;
+    }
+
+}
--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+let dollar;
+
 const listValues = async () => {
     //https://www.dolarsi.com/api/api.php?type=valoresprincipales Usar dps
     const dolarDia = await fetch('https://www.dolarsi.com/api/api.php?type=dolar');
@@ -5,17 +7,17 @@ const listValues = async () => {
     const dolarObj = await dolarDia.json();
     console.log("dolarDia: " + dolarObj[0].casa.compra)
     
-    let dolar = (parseFloat(dolarObj[0].casa.compra.replace(",", "."))).toFixed(2);
+    dollar = (parseFloat(dolarObj[0].casa.compra.replace(",", "."))).toFixed(2);
     
     let objDolar = document.getElementById("cotizacion");
-    objDolar.innerText = `Dólar Oficial hoy: AR$ ${dolar}`  
+    objDolar.innerText = `Dólar Oficial hoy: AR$ ${dollar}`  
 };
 
 window.addEventListener("load", function(){
     listValues();
 });
 
-let calcularPesos = () =>{
+let calculatePesos = () =>{
 
     const provinceNum = document.getElementById("provincia").value;
 
@@ -30,87 +32,65 @@ let calcularPesos = () =>{
     const pesosWoutTax= inputValue;
     valueList[0].innerText = `$${pesosWoutTax}`;
 
-    const countryTax = Number(inputValue*0.3.toFixed(3));
+    const countryTax = calculateCountryTax(inputValue)
     valueList[1].innerText = `$${countryTax}`;
 
-    let retGanBBPP;
-    if(moreThan300){
-        retGanBBPP = Number(inputValue*0.7.toFixed(3));
-        document.querySelector("span#ret").innerText = "70";
-        valueList[2].innerText = `$${retGanBBPP}`;
-    } else{
-        retGanBBPP = Number(inputValue*0.45.toFixed(3));
-        document.querySelector("span#ret").innerText = "45";
-        valueList[2].innerText = `$${retGanBBPP}`;
-    }
+    const retGanBBPP = calculateRetGanBPP(inputValue, moreThan300);
+    valueList[2].innerText = `$${retGanBBPP}`
 
     let provincePercent = document.querySelector("#perc");
 
-    let percIIBB;
+    const percIIBB = calculatePercIIBB(inputValue, provinceNum, provincePercent); 
+    valueList[3].innerText = `$${percIIBB}`
 
-    switch(provinceNum){
-        case "1":
-            percIIBB = 0.00
-            valueList[3].innerText = `$${percIIBB}`;
-            break;
-        case "2":
-        case "3":
-            percIIBB = Number(inputValue*0.02.toFixed(3));
-            valueList[3].innerText = `$${percIIBB}`;
-            provincePercent.innerText = 2;
-            break;
-        case "4":
-            percIIBB = Number(inputValue*0.03.toFixed(3));
-            valueList[3].innerText = `$${percIIBB}`;
-            provincePercent.innerText = 3;
-            break;
-        case "5":
-            percIIBB = Number(inputValue*0.01.toFixed(3));
-            valueList[3].innerText = `$${percIIBB}`;
-            provincePercent.innerText = 1;
-            break;
-        case "6":
-            percIIBB = Number(inputValue*0.05.toFixed(3));
-            valueList[3].innerText = `$${percIIBB}`;
-            provincePercent.innerText = 5;
-            break;
-        case "7":
-            percIIBB = Number(inputValue*0.036.toFixed(3));
-            valueList[3].innerText = `$${percIIBB}`;
-            provincePercent.innerText = 3.6;
-            break;
-        case "8":
-            percIIBB = Number(inputValue*0.055.toFixed(3));
-            valueList[3].innerText = `$${percIIBB}`;
-            provincePercent.innerText = 5.5;
-            break;
-        case "9":
-            percIIBB = Number(inputValue*0.03.toFixed(3));
-            valueList[3].innerText = `$${percIIBB}`;
-            provincePercent.innerText = 3;
-            break;
-    }
-
-    let sealTax;
-    if(payMethodNum=='1'){
-        sealTax = Number(inputValue*0.012.toFixed(3))
-        document.querySelector('span#sello').innerText = 1.2
-        valueList[4].innerText = `$${sealTax}`;
-    } else{
-        sealTax = 0.00;
-        document.querySelector('span#sello').innerText = 0
-        valueList[4].innerText = `$${sealTax}`;
-    }
+    const sealTax = calculateSealTax(inputValue, payMethodNum);
+    valueList[4].innerText = `$${sealTax}`
 
     const finalPrice = document.getElementById("total$");
     const totalTaxes = Number(pesosWoutTax)+Number(countryTax)+Number(retGanBBPP)+Number(percIIBB)+Number(sealTax);
     console.log(totalTaxes);
     finalPrice.innerText = `$${totalTaxes}`
-    return console.log(valueList);
+}
+
+let calculateDollars = () =>{
+
+    const provinceNum = document.getElementById("provincia").value;
+
+    const payMethodNum = document.getElementById("metodo-pago").value;
+
+    const moreThan300 = document.getElementById("si").checked;
+    
+    const inputValue = Number(document.getElementById("money").value).toFixed(3);
+
+    const valueList = document.querySelectorAll("#valores");
+
+    const pesosWoutTax= inputValue*dollar;
+    valueList[0].innerText = `$${pesosWoutTax}`;
+
+    const countryTax = calculateCountryTax(pesosWoutTax);
+    valueList[1].innerText = `$${countryTax}`;
+
+    const retGanBBPP = calculateRetGanBPP(pesosWoutTax, moreThan300);
+    valueList[2].innerText = `$${retGanBBPP}`
+
+    let provincePercent = document.querySelector("#perc");
+
+    const percIIBB = calculatePercIIBB(pesosWoutTax, provinceNum, provincePercent); 
+    valueList[3].innerText = `$${percIIBB}`
+
+    const sealTax = calculateSealTax(pesosWoutTax, payMethodNum);
+    valueList[4].innerText = `$${sealTax}`
+
+    const finalPrice = document.getElementById("total$");
+    const totalTaxes = Number(pesosWoutTax)+Number(countryTax)+Number(retGanBBPP)+Number(percIIBB)+Number(sealTax);
+    console.log(totalTaxes);
+    finalPrice.innerText = `$${totalTaxes}`
+
 }
 
 
-document.querySelector("button").addEventListener("click", calcularPesos);
+
+
 
 
 
